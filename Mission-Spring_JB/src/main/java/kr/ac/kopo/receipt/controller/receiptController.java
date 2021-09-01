51e2b0b822efb2bc7afa37d03acd4ebb14097c86
pjb <jb8049@naver.com>
2021-09-01 09:24:01 +0900

    file upload

--- Mission-Spring_JB/src/main/java/kr/ac/kopo/receipt/controller/receiptController.java
+++ Mission-Spring_JB/src/main/java/kr/ac/kopo/receipt/controller/receiptController.java
@@ -1,6 +1,8 @@
 package kr.ac.kopo.receipt.controller;
 
+import java.io.File;
 import java.util.Iterator;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -38,8 +40,6 @@ public class receiptController {
 		ModelAndView mav = new ModelAndView("receipt/receiptCheck");
 		
 		// 영수증 사진말고, 관련 없는 사진 등록하면 어떻게? => 템플릿 OCR에서는 구분가능함, general OCR에서는 어떻게?.. 잘못 올리는 사진에 대한 대비가 필요한데
-		// UploadController 참고
-		
 		
 		/*
 		 * 1. 파일을 서버 경로에 저장
@@ -48,6 +48,8 @@ public class receiptController {
 		 * 4. general ocr의 경우 합계 금액으로 아무것도 추출이 안되는 경우 -1을 반환하도록 만들어야함
 		 */
 		
+		String file_path = "C:\\Lecture\\spring-workspace\\newUpload\\";
+		System.out.println("파일 경로 : " + file_path);
 		
 		Iterator<String> iter = multipartRequest.getFileNames();
 		
@@ -59,6 +61,38 @@ public class receiptController {
 		String oriFileName = mFile.getOriginalFilename();
 		System.out.println("원본 파일명 : " + oriFileName);
 		
+		try {
+			
+			if(oriFileName != null && !oriFileName.equals("")) {
+				
+				// 확장자 처리
+				String ext = "";
+				
+				// 뒤쪽에 있는 . 의 위치 
+				int index = oriFileName.lastIndexOf(".");
+				
+				// 위 조건 .이 없으면 -1
+				if (index != -1) {
+					// 파일명에서 확장자명(.포함)을 추출
+					ext = oriFileName.substring(index);
+				}
+				
+				// 파일 사이즈
+				long fileSize = mFile.getSize();
+				System.out.println("파일 사이즈 : " + fileSize);
+				
+				// 고유한 파일명 만들기, 여러 명의 사용자가 동일한 이름의 파일을 올리는 경우 구분해주어야함
+				String saveFileName = "kopo-" + UUID.randomUUID().toString() + ext;
+				System.out.println("저장할 파일명 : " + saveFileName);
+				
+				// 임시저장된 파일을 내가 만들어둔 원하는 경로에 저장
+				mFile.transferTo(new File(file_path + saveFileName));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		
 		int selectedReceiptNo = Integer.parseInt(multipartRequest.getParameter("receiptKind"));
  		int selectedPurposeNo = Integer.parseInt(multipartRequest.getParameter("purpose"));
@@ -69,8 +103,8 @@ public class receiptController {
  		mav.addObject("selectedpurposeNo", selectedPurposeNo);
  		mav.addObject("inputMemo", inputMemo);
  		
- 		
 		return mav;
+		
 	}
 	
 	
